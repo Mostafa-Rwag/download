401bf1eb181e46f2f2e7c4aa8d8344c11a7abcdc
Mostafa-Rwag <104104339+Mostafa-Rwag@users.noreply.github.com>
2024-12-25 20:52:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
+import os
 from flask import Flask, render_template, request, send_file
 from pytube import YouTube
 import os
 
 app = Flask(__name__)
 
-# Path to save downloaded videos
 DOWNLOAD_FOLDER = "downloads"
 os.makedirs(DOWNLOAD_FOLDER, exist_ok=True)
 
@@ -15,18 +15,14 @@ def home():
 @app.route("/download", methods=["POST"])
 def download_video():
     try:
-        # Get the URL from the form
         video_url = request.form.get("url")
-        
-        # Download the video
         yt = YouTube(video_url)
         stream = yt.streams.get_highest_resolution()
         file_path = stream.download(output_path=DOWNLOAD_FOLDER)
-        
-        # Send the file to the user for download
         return send_file(file_path, as_attachment=True, download_name=f"{yt.title}.mp4")
     except Exception as e:
         return f"An error occurred: {e}"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
