a2302a6bb1258f7893869b937e3c82efc6a7b11e
Mostafa-Rwag <104104339+Mostafa-Rwag@users.noreply.github.com>
2024-12-27 18:15:01 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const { exec } = require('child_process');
+const { spawn } = require('child_process');
+
 const path = require('path');
 const fs = require('fs');
 const app = express();
